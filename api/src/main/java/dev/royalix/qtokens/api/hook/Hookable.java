2723a5889c586673861318a765848a147a1b7b63
oop-778 <oskardhavel@gmail.com>
2021-03-25 22:09:54 +0200

    More documentation changes

--- api/src/main/java/dev/royalix/qtokens/api/hook/Hookable.java
+++ api/src/main/java/dev/royalix/qtokens/api/hook/Hookable.java
@@ -1,8 +1,5 @@
 package dev.royalix.qtokens.api.hook;
 
-import dev.royalix.qtokens.api.hook.HookPriority;
-import dev.royalix.qtokens.api.hook.Hooked;
-import dev.royalix.qtokens.api.hook.ToolHook;
 import lombok.NonNull;
 
 public interface Hookable {
