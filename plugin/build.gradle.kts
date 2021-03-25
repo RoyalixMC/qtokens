2100eaf145eda6a68f99e9d54409754adefb340c
oop-778 <oskardhavel@gmail.com>
2021-03-25 22:22:42 +0200

    mavenLocal() -> HAHAHAHA

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -6,9 +6,13 @@ group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+    mavenLocal()
 }
 
 dependencies {
     implementation(project(":api"))
+
+    implementation("com.oop.inteliframework.config:1.0")
+    implementation("com.oop.inteliframework.config-node:1.0")
+    implementation("com.oop.inteliframework.config-property:1.0")
 }
