6d8174c3b006a8c9d9294a301e89fbfdf07bf286
oop-778 <oskardhavel@gmail.com>
2021-03-25 22:33:29 +0200

    impl. inteliframework properties as test (clean)

--- plugin/src/main/java/org/royalix/qtokens/plugin/config/main/MainConfig.java
+++ plugin/src/main/java/org/royalix/qtokens/plugin/config/main/MainConfig.java
@@ -1,4 +1,14 @@
 package org.royalix.qtokens.plugin.config.main;
 
-public class MainConfig {
+import com.oop.inteliframework.config.property.Configurable;
+import com.oop.inteliframework.config.property.annotations.Comment;
+import com.oop.inteliframework.config.property.annotations.Named;
+import com.oop.inteliframework.config.property.property.custom.ObjectProperty;
+
+@Comment({"Main configuration for Queen Pickaxes by Royalix", "Speed, Quality, Kebabs all in one!"})
+public class MainConfig implements Configurable {
+
+    @Named("tools-config")
+    private final ObjectProperty<ToolsPart> toolsPart = ObjectProperty.from(new ToolsPart());
+
 }
--- plugin/src/main/java/org/royalix/qtokens/plugin/config/main/ToolsPart.java
+++ plugin/src/main/java/org/royalix/qtokens/plugin/config/main/ToolsPart.java
@@ -1,9 +1,39 @@
 package org.royalix.qtokens.plugin.config.main;
 
-public class ToolsPart {
+import com.oop.inteliframework.config.property.Configurable;
+import com.oop.inteliframework.config.property.annotations.Comment;
+import com.oop.inteliframework.config.property.annotations.Named;
+import com.oop.inteliframework.config.property.property.PrimitiveProperty;
+import com.oop.inteliframework.config.property.property.custom.ObjectProperty;
 
-    private int maxTools;
-    private boolean droppable;
-    private DisplayOrder displayOrder;
+public class ToolsPart implements Configurable {
+    @Named("max-tools")
+    @Comment({
+            "Max amount of tools in a single inventory that a player can have?",
+            "If the player will receive another tool while he has reached the limit, it won't be picked up"
+    })
+    private final PrimitiveProperty.Mutable<Integer> maxTools = PrimitiveProperty.Mutable.fromInt(1);
 
+    @Named("global-tools")
+    @Comment({
+            "Should the tools be global?",
+            "If toggled, all players will be able to use that tools with the same enchants.",
+            "If off, each player will have own enchants, stats, etc. on the same tool"
+    })
+    private final PrimitiveProperty.Mutable<Boolean> globalTools = PrimitiveProperty.Mutable.fromBoolean(false);
+
+    @Named("droppable")
+    @Comment({
+            "Are players allowed to drop tools?"
+    })
+    private final PrimitiveProperty.Mutable<Boolean> droppable = PrimitiveProperty.Mutable.fromBoolean(false);
+
+    @Named("display-order")
+    @Comment({
+            "Display order of modules in tools",
+            "Separate each module by with a comma (,)"
+    })
+    private final ObjectProperty.Mutable<DisplayOrder> displayOrder = ObjectProperty.Mutable.from(DisplayOrder.ofString(
+            "enchants, levels, stats"
+    ));
 }
