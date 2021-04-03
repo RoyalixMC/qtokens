8dade699a3dcb1d050a0b1d6df7ec906efe2f233
oop-778 <oskardhavel@gmail.com>
2021-04-03 04:18:39 +0300

    Implemented deserialization

--- plugin/src/main/java/org/royalix/qtokens/plugin/inventory/QToolsInventory.java
+++ plugin/src/main/java/org/royalix/qtokens/plugin/inventory/QToolsInventory.java
@@ -1,11 +1,16 @@
 package org.royalix.qtokens.plugin.inventory;
 
+import de.tr7zw.changeme.nbtapi.NBTItem;
+import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
 import org.royalix.qtokens.api.inventory.ToolsInventory;
 import org.royalix.qtokens.api.tool.Tool;
 import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.royalix.qtokens.api.util.data.SerializedData;
+import org.royalix.qtokens.plugin.inventory.handler.ToolDeserializer;
 
 import java.util.HashSet;
 import java.util.Map;
@@ -14,11 +19,13 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class QToolsInventory implements ToolsInventory {
 
+    public static final String NBT_KEY = "QTOOL";
+
     @Getter
     private final Player owner;
     private final Map<Integer, Tool> toolsMap = new ConcurrentHashMap<>();
 
-    public QToolsInventory(Player owner) {
+    public QToolsInventory(@NonNull Player owner) {
         this.owner = owner;
     }
 
@@ -31,6 +38,20 @@ public class QToolsInventory implements ToolsInventory {
     public void initTools() {
         for (ItemStack content : owner.getInventory().getContents()) {
             if (content == null || content.getType() == Material.AIR) continue;
+
+            NBTItem nbtItem = new NBTItem(content);
+            if (!isTool(nbtItem))
+                continue;
+
+            // Init tool
+            SerializedData data = ToolDeserializer.get().deserialize(nbtItem);
         }
     }
+
+    public boolean isTool(NBTItem nbtItem) {
+        return nbtItem.getKeys()
+                .stream()
+                .anyMatch(key -> StringUtils.contains(key, NBT_KEY));
+    }
+
 }
