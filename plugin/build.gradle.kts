33e2cb751a26147634d5dfbb34c291193085c11c
oop-778 <oskardhavel@gmail.com>
2021-04-03 04:14:13 +0300

    added task, fast-uuid, platform, item as dependencies

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -12,7 +12,12 @@ repositories {
 dependencies {
     implementation(project(":api"))
 
-    implementation("com.oop.inteliframework.config:1.0")
-    implementation("com.oop.inteliframework.config-node:1.0")
-    implementation("com.oop.inteliframework.config-property:1.0")
+    implementation("com.oop.inteliframework:platform:1.0")
+    implementation("com.oop.inteliframework:config:1.0")
+    implementation("com.oop.inteliframework:item:1.0")
+    implementation("com.oop.inteliframework:task:1.0")
+    implementation("com.oop.inteliframework:task-bukkit:1.0")
+    implementation("com.oop.inteliframework:config-node:1.0")
+    implementation("com.oop.inteliframework:config-property:1.0")
+    implementation("com.eatthepath:fast-uuid:0.1")
 }
