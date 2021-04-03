2fa348223a8e2ecfe7e1c15083223c9c8bcc157f
oop-778 <oskardhavel@gmail.com>
2021-04-03 04:19:57 +0300

    Skeleton for MathExpression

---
+++ plugin/src/main/java/org/royalix/qtokens/plugin/util/math/MathExpression.java
@@ -0,0 +1,9 @@
+package org.royalix.qtokens.plugin.util.math;
+
+public class MathExpression {
+
+    public static MathExpression of(String input) {
+        return new MathExpression();
+    }
+
+}
